5d18398c26df108128600f919c60e8ca580d4bc2
Mostafa Osama <31553087+Battarshuck@users.noreply.github.com>
2023-06-25 22:23:36 +0300

    fix tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        learn react
+        mostafa
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         learn react
